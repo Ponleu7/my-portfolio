95f2f9edcc6d087e78a45fa67a683df283d1522e
ponleu <phonponleu7@gmail.com>
2024-03-21 14:48:10 +0700

    v3

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 import Hero from "@/components/Hero";
-import { Navbar } from "@/components/Navbar.1";
-import TypeWriter from "@/components/TypeWriter";
 import React from "react";
 export default function Home() {
   return (
